515be9a615b2c92d02415277ae1891f11a0baad1
Sam <enthunt.sh@gmail.com>
2026-02-28 17:35:21 -0500

    Add KeyStaff component and enhance Hero section with search functionality; update styles
    Signed-off-by: Sam <enthunt.sh@gmail.com>

--- oakGovClone/src/App.jsx
+++ oakGovClone/src/App.jsx
@@ -1,10 +1,12 @@
 import './styles/navbar.scss'
 import './styles/index.scss'
 import './styles/hero.scss'
+import './styles/contentSection.scss'
 import Greeting from './coreComponents/Greeting'
 import NavBar from './ContentComponents/NavBar'
 import Hero from './ContentComponents/Hero'
 import SideLinks from './ContentComponents/SideLinks'
+import KeyStaff from './ContentComponents/KeyStaff'
 
 function App() {
 
@@ -14,6 +16,12 @@ function App() {
         <NavBar></NavBar>
         <SideLinks></SideLinks>
         <Hero></Hero>
+
+        {/* Content Components */}
+        <KeyStaff></KeyStaff>
+        
+        
+        
       </Greeting>
     </>
   )
--- oakGovClone/src/ContentComponents/Hero.jsx
+++ oakGovClone/src/ContentComponents/Hero.jsx
@@ -1,11 +1,20 @@
 
 
-export default function Hero (){
+export default function Hero() {
   return (
     <main id="heroComponent">
       <div id="shadow"></div>
       <img src="/src/assets/imgs/heroImg.png" alt="" />
-
+      <div id="searchSection">
+        <div id="searchContainer">
+          <input type="text" placeholder="Search" />
+          <button id="heroSearch">🔎</button>
+        </div>
+        <span id="searchWOTD">
+          Top Searches: Property Records Inmate Locator Jurors Water Billing
+          Services
+        </span>
+      </div>
     </main>
-  )
+  );
 }

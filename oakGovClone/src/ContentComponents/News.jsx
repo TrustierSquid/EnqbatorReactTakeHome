a8f30e49f9cfd2f5281805450f04909a54567679
Sam <enthunt.sh@gmail.com>
2026-02-28 20:13:09 -0500

    Enhance News component with new layout and styling; add logo image and responsive design
    Signed-off-by: Sam <enthunt.sh@gmail.com>

--- oakGovClone/src/ContentComponents/News.jsx
+++ oakGovClone/src/ContentComponents/News.jsx
@@ -3,8 +3,45 @@
 export default function News() {
   return (
     <section id="newsContainer">
-      <h1>News</h1>
-      <p>Check back here for the latest news and updates from Oakland County.</p>
+      <h4 className="subtitle">KEEPING YOU INFORMED</h4>
+      <h1 className="title">News & Media</h1>
+      <div id="newsFlexContainer">
+
+        <span className="newsItem">
+          <div className="newsItemOverlayContainer">
+            <img src="/src/assets/imgs/logo.png" alt="logo" />
+            <span className="overlayMessage">Breaking!</span>
+          </div>
+          <span className="itemDate">🗓️ Feb 28</span>
+          <p className="itemDescription">Oakland County Expands Proactive Legionella Testing to Children’s Village as Results Show Preventive Measures Working.</p>
+        </span>
+        <span className="newsItem">
+          <div className="newsItemOverlayContainer">
+            <img src="/src/assets/imgs/logo.png" alt="logo" />
+            <span className="overlayMessage">Breaking!</span>
+          </div>
+          <span className="itemDate">🗓️ Feb 28</span>
+          <p className="itemDescription">Oakland County Expands Proactive Legionella Testing to Children’s Village as Results Show Preventive Measures Working.</p>
+        </span>
+
+
+        <span className="newsItem">
+          <div className="newsItemOverlayContainer">
+            <img src="/src/assets/imgs/logo.png" alt="logo" />
+          </div>
+          <span className="itemDate">🗓️ Feb 23</span>
+          <p className="itemDescription">Oakland County Expands Proactive Legionella Testing to Children’s Village as Results Show Preventive Measures Working.</p>
+        </span>
+        <span className="newsItem">
+          <div className="newsItemOverlayContainer">
+            <img src="/src/assets/imgs/logo.png" alt="logo" />
+          </div>
+          <span className="itemDate">🗓️ Feb 23</span>
+          <p className="itemDescription">Oakland County Expands Proactive Legionella Testing to Children’s Village as Results Show Preventive Measures Working.</p>
+        </span>
+        
+
+      </div>
     </section>
   )
 }

0a23a7fb09d5c36715edacee04935c40e16c307a
Sam <enthunt.sh@gmail.com>
2026-03-10 17:25:00 -0400

    Organized stylesheets and componets so that the respective componenets and stylesheets are paired with each other
    Signed-off-by: Sam <enthunt.sh@gmail.com>

--- oakGovClone/src/App.jsx
+++ oakGovClone/src/App.jsx
@@ -1,24 +1,35 @@
+// NavBar Components and styles
 import './styles/navbar.scss'
-import './styles/index.scss'
-import './styles/hero.scss'
-import './styles/contentSection.scss'
-import './styles/footer.scss'
-import './styles/businessComponent.scss'
-
-import Greeting from './coreComponents/Greeting'
 import NavBar from './ContentComponents/NavBar'
-import Hero from './ContentComponents/Hero'
 import SideLinks from './ContentComponents/SideLinks'
+
+// Hero Components and styles
+import './styles/hero.scss'
+import Hero from './ContentComponents/Hero'
+
+// Main Home page components and styles
+import './styles/contentSection.scss'
+import Greeting from './coreComponents/Greeting'
 import KeyStaff from './ContentComponents/KeyStaff'
 import Options from './ContentComponents/Options'
 import News from './ContentComponents/News'
 import CalendarOfEvents from './ContentComponents/CalendarOfEvents'
 import CountyLeaders from './ContentComponents/CountyLeaders'
+import Socials from './ContentComponents/Socials'
 import BlogsVideos from './ContentComponents/BlogsVideos'
 import Stats from './ContentComponents/Stats'
+
+// Footer components and styles
+import './styles/footer.scss'
 import Footer from './ContentComponents/Footer'
-import Socials from './ContentComponents/Socials'
+
+// Universal
+import './styles/index.scss'
+
+// Information Page components and styles
 import Information from './coreComponents/Information'
+import './styles/businessComponent.scss'
+
 
  import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
